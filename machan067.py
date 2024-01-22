69b1c9f74c6e485af4ba0039d57a50e184e88fdc
machanxx <156645372+machanxx@users.noreply.github.com>
2024-01-22 15:30:11 +0900

    Create machan067.py

---
+++ machan067.py
@@ -0,0 +1,15 @@
+import random
+
+class RandomAI(OthelloAI):
+    def __init__(self, face, name):
+        self.face = face
+        self.name = name
+
+    def move(self, board, color: int)->tuple[int, int]:
+        """
+        ボードが与えられたとき、どこに置くか(row,col)を返す
+        """
+        valid_moves = get_valid_moves(board, color)
+        # ランダムに選ぶ
+        selected_move = random.choice(valid_moves)
+        return selected_move
